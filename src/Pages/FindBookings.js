6122688ec000202cae68c2accfc8f53bee19d853
Haley Rutten <hrutten@uvic.ca>
2023-03-02 01:31:15 -0800

    added dynamic table and some dummy data i# Please enter the commit message for your changes. Lines starting

--- src/Pages/FindBookings.js
+++ src/Pages/FindBookings.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { Table } from "react-bootstrap";
+import ResultsTable from "../components/ResultsTable";
+import ResultsData from "../data/Resources.json";
 
 export function FindBookings() {
+  const column = [
+    { heading: "Resource", value: "type" },
+    { heading: "Date", value: "date" },
+    { heading: "Day", value: "day" },
+    { heading: "Time", value: "time" },
+  ];
+
   return (
-    <h1>Find Bookings</h1>
-  )
+    <div>
+      <h1>Find Bookings</h1>
+      <h2>Filter</h2>
+      <text>filters here</text>
+      <div>
+        <ResultsTable data={ResultsData} column={column} />
+      </div>
+    </div>
+  );
 }
---
+++ src/components/ResultsTable.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./resultstable.css";
+
+//taken from https://github.com/candraKriswinarto/dynamic-table
+
+//TODO: make button active and prettier
+
+const ResultsTable = ({ data, column }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          {column.map((item, index) => (
+            <TableHeadItem item={item} />
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item, index) => (
+          <TableRow item={item} column={column} />
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+const TableHeadItem = ({ item }) => <th>{item.heading}</th>;
+
+const TableRow = ({ item, column }) => (
+  <tr>
+    {column.map((columnItem, index) => {
+      if (columnItem.value.includes(".")) {
+        const itemSplit = columnItem.value.split(".");
+        return <td>item[itemSplit[0]][itemSplit[1]]</td>;
+      }
+      return <td>{item[`${columnItem.value}`]}</td>;
+    })}
+      <td><button>book</button></td>
+  </tr>
+
+);
+
+export default ResultsTable;

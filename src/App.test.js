7327aa1e12960182bee6dfe87e9278a3dbaa4b84
Fortnite-Burger <60914846+Fortnite-Burger@users.noreply.github.com>
2023-03-03 13:13:28 -0800

    change test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import "bootstrap/dist/css/bootstrap.min.css"
+import { BrowserRouter } from 'react-router-dom';
+ 
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  
 });
